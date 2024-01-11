43f9c1ca4843fc2bdc53216e4c28347f401ccbf2
andyfullstack <andriikoliadenko@gmail.com>
2024-01-11 11:12:03 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,21 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contactsFromLocalStorage = localStorage.getItem('contacts');
+    if (contactsFromLocalStorage) {
+      this.setState({
+        contacts: JSON.parse(contactsFromLocalStorage),
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
